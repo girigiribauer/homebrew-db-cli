9aee368eb88b6f209cd3b8802f06c1ed6a223306
girigiribauer <girigiribauer@gmail.com>
2016-10-01 22:47:34 +0900

    Initial commit

---
+++ db-cli.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+REPOSITORY_URL='https://github.com/girigiribauer/db-cli'
+HOMEBREW_DBCLI_VERSION='0.1.24'
+
+class DBCli < Formula
+  desc '`db` command line tools (Docker based)'
+  homepage REPOSITORY_URL
+  url "#{REPOSITORY_URL}/releases/download/v#{HOMEBREW_DBCLI_VERSION}/db-darwin-amd64.tar.gz", :tag => "v#{HOMEBREW_DBCLI_VERSION}"
+  sha256 '6bfc2486db1b17ab93d3b3783f5981b3676299319ff9208efd599a4843599a06'
+  head REPOSITORY_URL, :branch => 'master'
+  version HOMEBREW_DBCLI_VERSION
+
+  def install
+    bin.install 'db'
+  end
+end
